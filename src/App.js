e2ddcabc59bf51731fbf5b7afc5617ee4877d781
Matheus Soutto <matheussoutto27@gmail.com>
2020-09-23 23:37:39 -0300

    separating authentication logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import dotenv from "dotenv";
-import axios from "axios";
 import React, { Component } from "react";
 import {
   BrowserRouter as Router,
-  Redirect,
   Route,
   Switch
 } from "react-router-dom";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import Home from "./components/pages/Home";
 import Search from "./components/pages/Search";
 import About from "./components/pages/About";
 import Navbar from "./components/layout/Navbar";
@@ -17,60 +13,22 @@ import NotFound from "./components/pages/NotFound";
 import Add from "./components/deliverymen/Add";
 import Edit from "./components/deliverymen/Edit";
 import View from "./components/deliverymen/View";
-
-dotenv.config();
-
-const login = async () => {
-    return await axios({
-    method: 'post',
-    url: process.env.API_URL + '/auth/authenticate',
-    data: { login: process.env.API_USER, password: process.env.API_PASSWORD },
-    headers: {
-      'content-type': 'application/json'
-    }
-  });
-}
+import TakePhoto from "./components/common/TakePhoto";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={props =>
-      localStorage.getItem('auth') ? (
+      localStorage.getItem('token') ? (
         <Component {...props} />
       ) : (
-          <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+          <NotFound  />
         )
     }
   />
 );
 
 class App extends Component {
-
-  constructor() {
-    super();
-
-    if (!localStorage.getItem('auth')) {
-      login().then(result => {
-        if (result && result.token && result.token.length > 0) {
-          localStorage.setItem('auth', result);
-          console.log(localStorage.getItem('auth'));
-          // Retrieve the last state
-          this.state = localStorage.getItem('auth') ? localStorage.getItem('auth') : undefined;
-        }
-      })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-
-
-  }
-
-  componentWillUnmount() {
-    // Remember state for the next mount
-    localStorage.setItem('auth', this.state);
-  }
-
   render() {
     return (
       <Router>
@@ -78,12 +36,13 @@ class App extends Component {
           <Navbar />
 
           <Switch>
-            <PrivateRoute exact path="/" component={Home} />
-            <PrivateRoute exact path="/search" component={Search} />
+            <PrivateRoute exact path="/" component={Search} />
+            {/*<PrivateRoute exact path="/search" component={Search} />*/}
             <PrivateRoute exact path="/about" component={About} />
-            <PrivateRoute exact path="/deliverymen/add" component={Add} />
+            <PrivateRoute exact path="/deliverymen" component={Add} />
             <PrivateRoute exact path="/deliverymen/edit/:id" component={Edit} />
             <PrivateRoute exact path="/deliverymen/:id" component={View} />
+            <PrivateRoute exact path="/photo" component={TakePhoto} />
             <PrivateRoute component={NotFound} />
           </Switch>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Api from './config';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const login = async () => {
+  return await axios({
+    method: 'post',
+    url: Api.endpoint + '/auth/authenticate',
+    data: { login: Api.login, password: Api.password },
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
+}
+
+login().then(result => {
+  console.log('logou');
+  if (result.data && result.data.token) {
+    console.log(result.data);
+    localStorage.setItem('token', result.data.token);
+    localStorage.setItem('userId', result.data.user._id);
+  }
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+})
+  .catch(err => {
+    console.log(err);
+  });
 
