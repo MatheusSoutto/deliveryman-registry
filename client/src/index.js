e628ef1273f9e0a1b6fabad28c01f01e96e1ad80
Matheus Soutto <matheussoutto27@gmail.com>
2020-09-26 15:08:57 -0300

    dockerizing project

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,17 @@ import './index.css';
 import Api from './config';
 import App from './App';
 
+const register = async () => {
+  return await axios({
+    method: 'post',
+    url: Api.endpoint + '/auth/register',
+    data: { login: Api.login, password: Api.password },
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
+}
+
 const login = async () => {
   return await axios({
     method: 'post',
@@ -23,15 +34,23 @@ login().then(result => {
     localStorage.setItem('token', result.data.token);
     localStorage.setItem('userId', result.data.user._id);
   }
-
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
 })
   .catch(err => {
     console.log(err);
+    register().then(result => {
+      console.log('logou');
+      if (result.data && result.data.token) {
+        console.log(result.data);
+        localStorage.setItem('token', result.data.token);
+        localStorage.setItem('userId', result.data.user._id);
+      }
+    })
+  })
+  .finally(() => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
   });
-
