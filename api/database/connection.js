c2562a05e29f28932b714b4f3ecd1ed931640a1f
Matheus Soutto <matheussoutto27@gmail.com>
2020-09-26 14:18:00 -0300

    setting api to connect to local mongodb

--- api/database/connection.js
+++ api/database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 try {
-  mongoose.connect('mongodb://matheus-soutto:Matheus.13@hackerrank-shard-00-00.jyajn.mongodb.net:27017,hackerrank-shard-00-01.jyajn.mongodb.net:27017,hackerrank-shard-00-02.jyajn.mongodb.net:27017/Deliverymen?ssl=true&replicaSet=HackerRank-shard-0&authSource=admin&retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+  mongoose.connect('mongodb://mongo:27017', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
   mongoose.Promise = global.Promise;
 }
 catch(error) {
